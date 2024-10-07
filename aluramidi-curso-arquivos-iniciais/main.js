46bb19e36cfc1f7aa4f695028ef3eac65c239faf
vanessabispo <wheidybispo1992@gmail.com>
2024-10-07 20:51:07 -0300

    primeiro commit

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,52 @@
+
+function tocaSom(idElementoAudio){
+
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+//enquanto//
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown = function(evento){
+
+        if (evento.code === 'Space' || evento.code ==='Enter'){
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
+}
+
+
+
+/*while(contador < listaDeTeclas.length){
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function (){
+    tocaSom(idAudio);
+  }
+
+  contador = contador + 1;
+
+}*/
